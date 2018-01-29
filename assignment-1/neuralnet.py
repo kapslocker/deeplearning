Fix backprop

assignment-1/neuralnet.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 from functions import *
+from operator import add
 class NeuralNetwork(object):
     def __init__(self, learningRate, model, minibatchsize, epochs, activation_function = 'sigmoid', activation_function_grad = 'sigmoid_grad'):
         ''' Setup a fully connected neural network represented by
@@ -11,7 +12,7 @@ class NeuralNetwork(object):
         self.num_layers = len(model)
         self.epochs = epochs
         self.rate = learningRate
-        self.minibatchsize = minibatchsize
+        self.mini_batch_size = minibatchsize
         if activation_function == 'sigmoid':
             self.activation_function = lambda x : sigmoid(x)
             self.activation_function_grad = lambda x : sigmoid_grad(x)
@@ -40,35 +41,49 @@ class NeuralNetwork(object):
         return np.argmax(self.activations[-1])
 
     def forwardProp(self, x):
-        self.activations[0] = self.activation_function(x)
+        self.activations[0] = x
         for i in xrange(1, self.num_layers):
-            self.z[i] = self.weights[i].dot(self.activations[i - 1]) + self.biases[i]
+            self.z[i] = np.dot(self.weights[i], self.activations[i - 1]) + self.biases[i]
             self.activations[i] = self.activation_function(self.z[i])
 
     def learn(self, training_data, test_data):
-    	random.shuffle(training_data)
+        self.N = len(training_data)
+        ''' Minibatch gradient descent '''
         for i in xrange(self.epochs):
-            for x, y in training_data:
-               self.forwardProp(x)
-               delta_biases, delta_weights = self.error(x, y)
-               self.biases =  [b - self.rate * d_b for b, d_b in zip(self.biases, delta_biases)]
-               self.weights = [w - self.rate * d_w for w, d_w in zip(self.weights, delta_weights)]
-            print i, self.test(test_data)
+    	       random.shuffle(training_data)
+               ''' create batches by choosing randomly'''
+               batches = [training_data[j : j + self.mini_batch_size] for j in xrange(0, self.N, self.mini_batch_size)]
+               for batch in batches:
+                   self.updatebatch(batch)
+               print "Processed Epoch {0} ".format(i),
+               self.test(test_data)
 
+    def backprop(self, x, y):
+        error_biases =  [np.zeros(bias.shape) for bias in self.biases]
+        error_weights = [np.zeros(wt.shape) for wt in self.weights]
+        ''' One forward pass followed by one backward pass '''
+        self.forwardProp(x)
+        outputLayerError = (self.activations[-1] - y) * self.activation_function_grad(self.z[-1])
+        error_biases[-1] = outputLayerError
+        error_weights[-1] = outputLayerError.dot(self.activations[-2].transpose())
+        for i in xrange(self.num_layers - 2, 0, -1):
+            temp = np.dot(self.weights[i + 1].transpose(), error_biases[i + 1]) * self.activation_function_grad(self.z[i])
+            error_biases[i] = temp
+            error_weights[i] = np.dot(error_biases[i], np.transpose(self.activations[i - 1]))
+        return error_biases, error_weights
 
+    def updatebatch(self, batch):
+        error_biases = [np.zeros(bias.shape) for bias in self.biases]
+        error_weights = [np.zeros(weight.shape) for weight in self.weights]
+        ''' Evaluate gradients for each image in batch '''
+        for x,y in batch:
+            db, dw = self.backprop(x,y)
+            error_biases = map(add, error_biases, db)
+            error_weights = map(add, error_weights, dw)
+        ''' Update weights and bias '''
+        self.biases = [bias - (self.rate * error_bias) / self.mini_batch_size for bias, error_bias in zip(self.biases, error_biases)]
+        self.weights = [weight - (self.rate * error_weight) / self.mini_batch_size for weight, error_weight in zip(self.weights, error_weights)]
     def test(self, test_data):
         n = len(test_data)
         count = len(filter(lambda (x,y) : np.argmax(y) == self.predict(x), test_data))
-        return (float(count) * 100.0) / float(n)
-
-    def error(self, x, y):
-    	error_biases = [np.zeros(bias.shape) for bias in self.biases]
-    	error_weights = [np.zeros(weight.shape) for weight in self.weights]
-        error_biases[-1] = (self.activations[-1] - y) * self.activation_function_grad(self.z[-1])
-    	error_weights[-1] = np.transpose(self.activations[-2] * np.transpose(error_biases[-1]))
-    	for i in xrange(self.num_layers-2,0,-1):
-            temp = np.dot(np.transpose(error_weights[i + 1]), error_biases[i + 1])
-            error = np.multiply(temp, self.activation_function_grad(self.z[i]))
-            error_biases[i] = error
-            error_weights[i] = np.transpose(self.activations[i-1] * np.transpose(error))
-    	return error_biases, error_weights
+        print count, '/', n