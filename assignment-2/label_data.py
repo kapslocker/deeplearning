58f35bedfd57afaa3dcb73b7671058940bcf5c73
Kapil Ahuja <kapil99a@gmail.com>
2018-03-20 19:52:17 +0530

    generate noise labels

--- assignment-2/label_data.py
+++ assignment-2/label_data.py
@@ -12,21 +12,32 @@ if __name__ == '__main__':
     parser.add_argument("tag", type = str, help = "the category to which this video goes")
     args = parser.parse_args()
 
-    folder = "data/" + args.tag + "/"
+    ROOT = "data/"
+    folder = ROOT + args.tag + "/"
+    noise_folder = ROOT + "noise/"
+    labelfile = ROOT + "labels.txt"
     if not os.path.exists(folder):
         os.makedirs(folder)
+    if not os.path.exists(noise_folder):
+        os.makedirs(noise_folder)
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     cap = cv2.VideoCapture(args.vid_file)
     print "Video length = " + str(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     frame_num = 0
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        grayframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(grayframe, 1.3, 5)
-        if(len(faces) == 1):
+    with open(labelfile, 'a') as labels:
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            grayframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(grayframe, 1.3, 5)
             frame_num += 1
-            cv2.imwrite(folder + str(frame_num) + ".jpg", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
+            if(len(faces) == 1):
+                path = folder + str(frame_num) + ".jpg"
+                label = path + "\t" + args.tag + "\n"
+            else:
+                path = noise_folder + args.tag + "_" + str(frame_num) + ".jpg"
+                label = path + "\t" + "noise" + "\n"
+            labels.write(label)
+            cv2.imwrite(path, frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cap.release()
