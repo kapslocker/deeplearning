789a6a70aaad1f0e531efbb6d981a853bb9e86dc
Kapil Ahuja <kapil99a@gmail.com>
2018-04-02 19:52:08 +0530

    Merging changes

--- assignment-2/label_data.py
+++ assignment-2/label_data.py
@@ -4,16 +4,15 @@ import numpy as np
 import argparse
 
 ''' Generate dataset:
-    1. Filter video to get frames that have 1 face. The detection is done at a rate of 1 frame per second.
-    2. Faces are identified using the Viola Jones face detector in OpenCV.
-    3. Save frames and generate txt with labels.'''
+    1. Filter video to get frames that have 1 speaker.
+    2. Save frames and generate txt with labels.'''
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("vid_file", type = str, help = "video location")
     parser.add_argument("tag", type = str, help = "the category to which this video goes")
     args = parser.parse_args()
     print args.vid_file
-    ROOT = "data_sadguru/"
+    ROOT = "data/"
     CROPPED_ROOT = "data_cropped/"
     folder = ROOT + args.tag + "/"
     folder2 = CROPPED_ROOT + args.tag + "/"
@@ -25,7 +24,7 @@ if __name__ == '__main__':
         os.makedirs(folder2)
     if not os.path.exists(noise_folder):
         os.makedirs(noise_folder)
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     cap = cv2.VideoCapture(args.vid_file)
     vidname = args.vid_file.replace(' ','').replace('-','').replace('.','').replace('/','').replace('!','').replace('&','')
     print "Video length = " + str(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -38,29 +37,22 @@ if __name__ == '__main__':
                 frame_num += 1
                 if(frame_num % 20 == 0):
                     grayframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                    faces = face_cascade.detectMultiScale(grayframe)
+                    faces = face_cascade.detectMultiScale(grayframe, 1.3, 5)
                     cropped_label = "-"
-                    ''' Get the center of the frame '''
-                    r,c = grayframe.shape
-
-                    # grayframe = grayframe[:, 700 : c]
-                    # print grayframe.shape
-                    if(len(faces) > 1):
-                        N,M = grayframe.shape
-                        (x1,y1,w1,h1) = faces[0]
-                        (x2, y2, w2, h2) = faces[1]
+                    if(len(faces) == 1):
+                        (x,y,w,h) = faces[0]
+                        path = folder + vidname + str(frame_num) + ".jpg"
+                        path2 = folder2 + vidname + str(frame_num) + ".jpg"
+                        label = path + "\t" + args.tag + "\n"
+                        cropped_label = path2 + "\t" + args.tag + "\n"
+                    else:
+                        path = noise_folder + vidname + "_" + str(frame_num) + ".jpg"
+                        label = path + "\t" + "noise" + "\n"
+                    if cropped_label is not "-":
                         labels_cropped.write(cropped_label)
-                        path2 = folder2 + vidname + str(frame_num) + '.jpg'
-                        x = (x1 + x2) / 2
-                        width = max(x1, x2) - min(x1,x2) + 50 + w1 + w2
-                        height = int(1.618 * width)
-                        subset = frame[max(y1 - height / 2, 0) : min(y1 + height / 2, N), max(x - width/2, 0) : min(x + width / 2, M)]
-                        r,c, d = subset.shape
-                        # cv2.imshow('asd', subset)
-                        # # print float(r) / float(c)
-                        if(r > 500 or abs(c - 500) < 200):
-                            # cv2.imshow('asd', subset)
-                            cv2.imwrite(path2, subset)
+                        cv2.imwrite(path2, frame[y: y + h, x : x + w])
+                    labels.write(label)
+                    cv2.imwrite(path, frame)
                     if cv2.waitKey(1) & 0xFF == ord('q'):
                         break
     cap.release()
