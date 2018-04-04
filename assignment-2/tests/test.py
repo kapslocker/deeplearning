fa0a1ff94ff1e1b83853b6a8a6c4b59ad7c8f36a
Kapil Ahuja <kapil99a@gmail.com>
2018-04-04 17:29:27 +0530

    Update train files, fetch outputs from HPC

--- assignment-2/tests/test.py
+++ assignment-2/tests/test.py
@@ -11,7 +11,8 @@ import argparse
 
 def get_faces(frame):
     grayframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier('../classifier/' + 'cascade.xml')
+    # face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     face_roi = face_cascade.detectMultiScale(grayframe, 1.3, 5)
     faces = [frame[y:y + h, x: x + w] for (x,y,w,h) in face_roi]
     return faces, face_roi
@@ -31,19 +32,30 @@ def to_tensor(frame):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("img_file", type = str, help = "image location")
+    parser.add_argument("img_file", type = str, help = "Test image\\video location")
+    parser.add_argument("saved_model", type = str, help = "Saved model file location")
     args = parser.parse_args()
-    num_classes = 6
-    labels = ['atul', 'raman', 'sadguru', 'sandeep', 'shailendra', 'sorabh']
+    num_classes = 7
+    labels = ['atul', 'noise', 'raman', 'sadguru', 'sandeep', 'shailendra', 'sorabh']
     if(torch.cuda.is_available()):
-        model = torch.load('models/saved_model.pth')
+        model = torch.load(args.saved_model)
     else:
-        model = torch.load('models/saved_model.pth', map_location = lambda storage, loc: storage)
+        model = torch.load(args.saved_model, map_location = lambda storage, loc: storage)
     model.train(False)
 
     cap = cv2.VideoCapture(args.img_file)
     while(True):
         code, frame = cap.read()
+        # Uncomment for direct checking without face detection.
+        # output = model(to_tensor(Image.fromarray(frame)))
+        # temp = frame
+        # font = cv2.FONT_HERSHEY_SIMPLEX
+        # idx = np.argmax(output.data.numpy())
+        # cv2.putText(temp, labels[idx], (100 - 50, 100 - 50), font, 4, (255,255,255), 2, cv2.LINE_AA)
+        # cv2.imshow('result', temp)
+        # cv2.waitKey(1)
+
+        # Uncomment for checking after faces are detected.
         faces, locs = get_faces(frame)
         if(len(faces) == 0):
             cv2.imshow('result', frame)
@@ -56,6 +68,6 @@ if __name__ == '__main__':
             font = cv2.FONT_HERSHEY_SIMPLEX
             cv2.rectangle(temp, (x,y), (x + w, y + h), (255,0,255), 2)
             idx = np.argmax(output.data.numpy())
-            cv2.putText(temp, labels[idx], (x - 50, y - 50), font, 4, (255,255,255), 2, cv2.LINE_AA)
+            cv2.putText(temp, labels[idx], (x , y), font, 1, (255,255,255), 2, cv2.LINE_AA)
             cv2.imshow('result', temp)
             cv2.waitKey(1)
--- assignment-2/train.py
+++ assignment-2/train.py
@@ -9,45 +9,30 @@ import torchvision
 from prepare_data import *
 import os
 import copy
+import argparse
 
-data_dir = 'data_cropped'
-splits = ['train', 'val']
-dataset = fetch_data(data_dir, splits)
-dataloaders = {x: torch.utils.data.DataLoader(dataset[x], batch_size=4, shuffle=True, num_workers=4) for x in splits}
-dataset_sizes = {x: len(dataset[x]) for x in splits}
-num_classes = len(dataset['train'].classes)
-class_names = dataset['train'].classes
 
-use_gpu = torch.cuda.is_available()
-
-# Fine tuning the pre-trained network
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-
     best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
-
-    for epoch in range(num_epochs):
+    for epoch in xrange(num_epochs):
+        print()
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
-
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
+        ''' Train and test in every epoch. (To get stats after every epoch). '''
+        for phase in ['train', 'test']:
             if phase == 'train':
                 scheduler.step()
-                model.train(True)  # Set model to training mode
+                model.train(True)
             else:
-                model.train(False)  # Set model to evaluate mode
-
+                model.eval()
             running_loss = 0.0
             running_corrects = 0
 
-            # Iterate over data.
             for data in dataloaders[phase]:
+                ''' Run over data (SGD) '''
                 inputs, labels = data
-                # wrap them in Variable
                 inputs = Variable(inputs.cuda())
                 labels = Variable(labels.cuda())
-                # zero the parameter gradients
                 optimizer.zero_grad()
                 # forward
                 outputs = model(inputs)
@@ -63,39 +48,69 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects / dataset_sizes[phase]
 
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+            print('Phase: {}, Loss: {} Acc: {}'.format(
                 phase, epoch_loss, epoch_acc))
 
-            # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
+            ''' Save the best model '''
+            if phase == 'test' and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best_model_wts = copy.deepcopy(model.state_dict())
         print()
 
-    print('Best val Acc: {:4f}'.format(best_acc))
-    # load best model weights
+    print('best test accuracy: {}'.format(best_acc))
     model.load_state_dict(best_model_wts)
     return model
 
+if __name__ == '__main__':
+    ''' Fine tuning three networks (Transfer Learning) by adding a FC layer'''
+    ''' Load dataset '''
+    data_dir = 'data'
+    splits = ['train', 'test']
+    dataset = fetch_data(data_dir, splits)
+    dataloaders = {x: torch.utils.data.DataLoader(dataset[x], batch_size=4, shuffle=True, num_workers=4) for x in splits}
+    dataset_sizes = {x: len(dataset[x]) for x in splits}
+    num_classes = len(dataset['train'].classes)
+    class_names = dataset['train'].classes
 
-######################################################################
-# Finetuning the convnet (Inception v3)
-# ----------------------
-model_ft = models.resnet18(pretrained=False)
-model_ft.load_state_dict(torch.load('resnet18-5c106cde.pth'))
-num_ftrs = model_ft.fc.in_features
-model_ft.fc = torch.nn.Linear(num_ftrs, num_classes)
+    use_gpu = torch.cuda.is_available()
 
-model_ft = model_ft.cuda()
-criterion = torch.nn.CrossEntropyLoss()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model', type = str, help = "Which pretrained model to use: (\'resnet\', \"vgg16\", \'alexnet\')")
+    args = parser.parse_args()
+    if args.model == 'resnet':
+        ''' Downloaded from: https://download.pytorch.org/models/resnet18-5c106cde.pth '''
+        model_ft = models.resnet18(pretrained=False)
+        model_ft.load_state_dict(torch.load('models/resnet18-5c106cde.pth'))
+    elif args.model == 'vgg16':
+        ''' Downloaded from: https://download.pytorch.org/models/vgg16-397923af.pth '''
+        model_ft = models.vgg16(pretrained = False)
+        model_ft.load_state_dict(torch.load('models/vgg16-397923af.pth'))
+    elif args.model == 'alexnet':
+        ''' Downloaded from https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth '''
+        model_ft = models.alexnet(pretrained = False)
+        model_ft.load_state_dict(torch.load('models/alexnet-owt-4df8aa71.pth'))
 
-# Observe that all parameters are being optimized
-optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+    else:
+        print("Invalid choice of model")
+        exit(0)
 
-# Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+    ''' Add a fc layer to the model '''
+    if args.model == 'vgg16':
+        num_ftrs = model_ft.classifier[0].out_features
+    else:
+        num_ftrs = model_ft.fc.in_features
+    model_ft.fc = torch.nn.Linear(num_ftrs, num_classes)
 
-model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                       num_epochs=25)
-# Save model to file
-torch.save(model_ft, 'saved_model.pth')
+    model_ft = model_ft.cuda()
+    ''' Train to minimize the cross entropy loss '''
+    criterion = torch.nn.CrossEntropyLoss()
+
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+    ''' Exponential decay of learning rate by a factor of 0.1 every 5 epochs '''
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=5, gamma=0.1)
+
+    model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                           num_epochs=25)
+    ''' Save best model to file '''
+    torch.save(model_ft, 'models/saved_model_' + args.model + '.pth')
